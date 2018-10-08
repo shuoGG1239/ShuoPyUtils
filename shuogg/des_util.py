b2360f3a7b1b0277b254a157f8658e1ddbc90d52
shuoGG <328893769@qq.com>
2018-10-08 09:18:05 +0800

    update des_util.py

--- shuogg/des_util.py
+++ shuogg/des_util.py
@@ -3,6 +3,6 @@ from pyDes import *
 
 
 def encrypt(key, content):
-    k = des(key.encode())
+    k = des(key.encode(), padmode=PAD_PKCS5)
     des_bytes = k.encrypt(content)
     return base64.b64encode(des_bytes).decode('ascii')
