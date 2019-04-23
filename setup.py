c36106ea65103e72de25b41c48aac0bf54dc1de2
linshuo <linshuo@kingsoft.com>
2019-04-23 20:27:55 +0800

    add deploy

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ setup(
     name="shuogg",              # 包名
     version="0.1",              # 版本信息
     packages=['shuogg'],        # 要打包的项目文件夹
+    description="shuoGG's common utils",
+    keywords='python util',
+    author='shuoGG',
+    author_email='328893769@qq.com',
+    url='https://github.com/shuoGG1239/ShuoPyUtils',
+    zip_safe=False
 )
